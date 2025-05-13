ac678cc55a825811b9001029762527eae8b5463b
sinweaver2089 <144281351+sinweaver2089@users.noreply.github.com>
2025-05-13 12:47:02 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KahootBotJoiner from './components/KahootBotJoiner';
+
+const App = () => {
+  return (
+    <div>
+      <KahootBotJoiner />
+    </div>
+  );
+};
+
+export default App;
